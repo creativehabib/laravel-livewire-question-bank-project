628777964130c68120b55578919cfe29e6b7f09e
creativehabib <shamolrahaman@gmail.com>
2025-08-30 01:36:02 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,6 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-    // এই অংশটুকু যোগ করুন
-    safelist: [
-        'justify-end',
-        'justify-start',
-        'bg-indigo-600',
-        'text-white',
-        'bg-gray-200',
-        'dark:bg-gray-700',
-        'text-gray-800',
-        'dark:text-gray-100',
-        'ml-2',
-        'mr-2'
-    ],
     darkMode: 'class', // ⬅️ এটা অবশ্যই থাকতে হবে
     theme: {
         extend: {
