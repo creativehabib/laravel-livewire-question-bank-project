8a22ddbbc857511fefe6d6ff50d66b992193a417
creativehabib <shamolrahaman@gmail.com>
2025-09-07 21:07:38 +0600

    update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
-import Echo from 'laravel-echo/dist/echo.esm.js';
+// import Echo from 'laravel-echo';
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    forceTLS: false,
-    enabledTransports: ['ws', 'wss'],
-});
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+//     forceTLS: false,
+//     enabledTransports: ['ws', 'wss'],
+// });
